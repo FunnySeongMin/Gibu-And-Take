bd7e2c7bdd85f7219907cb8cf37c5f67a2e14141
minkyeng <minkyeng@192.168.0.142>
2018-05-23 09:42:41 +0900

    feat : 해당 재능기부에 대한 후기 목록, 응원메시지 페이징 적용
    1. 해당 재능기부에 대한 후기 목록(페이징 적용)
    -Model
    ---DonationDAO
    ---DonationDAOImpl : ReviewPostListVO readDonationReviewList(String
    dpno, int nowPage)
    ---DonationService
    ---DonationServiceImpl : ReviewPostListVO readDonationReviewList(String
    dpno,int nowPage)
    -Controller
    ---DonationController : 페이징이 적용된 해당 재능기부에 대한 응원메시지와 후기 목록
    -mapper
    ---Donation.xml
    
    2. 해당 재능기부에 대한 응원메시지 목록(페이징 적용)
    -Model
    ---EntryDAO
    ---EntryDAOImpl
    : TakeDonationPostListVO findCheerupMessageByDpno(String dpno, int
    nowPage)
    : TakeDonationPostVO findEntryByIdAndDpno(TakeDonationPostVO tdVO) -
    재능기부 참여여부
    ---EntryService
    ---EntryServiceImpl
    : TakeDonationPostListVO findCheerupMessageByDpno(String dpno, int
    nowPage)
    : TakeDonationPostVO findEntryByIdAndDpno(TakeDonationPostVO tdVO) -
    재능기부 참여여부
    -Controller
    ---DonationController : 페이징이 적용된 해당 재능기부에 대한 응원메시지와 후기 목록
    -mapper
    ---Entry.xml
    
    
    3. 기타
    - 재능기부 view
    ---listDonation
    : 해당 재능기부 상세보기를 위한 링크 수정
    ---readDonationDetail
    : 참여하기 버튼 예외처리(비로그인 회원)
    : 후기 버튼 예외처리(참여한 회원만 작성 가능하도록)
    : 해당 재능기부에 대한 후기목록, 응원메시지목록 paging
    -코드정리
    ---EntryController
    -label style 추가
    ---gat-style.css

--- gat/src/main/java/org/kosta/gat/model/service/DonationServiceImpl.java
+++ gat/src/main/java/org/kosta/gat/model/service/DonationServiceImpl.java
@@ -33,6 +33,7 @@ public class DonationServiceImpl implements DonationService {
 		return donationDAO.readDonationList(nowPage);
 	}
 
+	// 해당 재능기부 상세보기
 	@Override
 	public DonationPostVO readDonationDetail(String dpno) {
 		return donationDAO.readDonationDetail(dpno);
@@ -43,6 +44,7 @@ public class DonationServiceImpl implements DonationService {
 		return donationDAO.readDonationCheerUpMessageList(dpno);
 	}
 	
+	// 해당 재능기부 후기 목록
 	@Override
 	public ReviewPostListVO readDonationReviewList(String dpno,int nowPage) {
 		return donationDAO.readDonationReviewList(dpno,nowPage);
@@ -102,10 +104,5 @@ public class DonationServiceImpl implements DonationService {
 	@Override
 	public List<Map<String, Object>> DonationListRank() {
 		return donationDAO.DonationListRank();
-	}
-	
-	@Override
-	public List<ReviewPostVO> readDonationReviewList(String dpno) {
-		return donationDAO.readDonationReviewList(dpno);
 	}
 }
